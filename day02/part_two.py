3931675d2101b9e0c7bfe6cf64f5030319a77fe1
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2020-12-02 10:24:42 +0000

    Day two

---
+++ day02/part_two.py
@@ -0,0 +1,24 @@
+
+def main():
+    with open("data.txt", "r") as f:
+        lines = [
+            line.replace("-", " ").replace(":", "").replace("\n", "").split(" ")
+            for line in f.readlines()
+        ]
+
+    valid_passwords = []
+
+    for position1, position2, letter, password in lines:
+        index1 = int(position1) - 1
+        index2 = int(position2) - 1
+
+        if password[index1] == letter and password[index2] != letter:
+            valid_passwords.append(password)
+        elif password[index1] != letter and password[index2] == letter:
+            valid_passwords.append(password)
+
+    print(len(valid_passwords))
+
+
+if __name__ == '__main__':
+    main()
