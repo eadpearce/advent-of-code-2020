b9d3d1054ac8575426f274ee772f2ffa8d75efa2
eadpearce <e.pearce@tutanota.com>
2020-12-05 15:30:57 +0000

    Tidying

--- day05/part_two.py
+++ day05/part_two.py
@@ -1,11 +1,8 @@
-from decoder import get_coords, get_id
+from decoder import get_id
 
 
 with open("data.txt", "r") as f:
     codes = [code.replace('\n', '') for code in f.readlines()]
-
-rows = list(set([get_coords(code)[0] for code in codes]))
-cols = list(set([get_coords(code)[1] for code in codes]))
 
 seats = sorted([get_id(code) for code in codes])
 
